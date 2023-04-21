74a60d4f7cfc698f2725e522bc92e9d0abb5bbdd
Chris King <cking91977@gmail.com>
2023-04-21 15:50:25 -0700

    fix(meta): use correct namespace and description

--- src/meta.js
+++ src/meta.js
@@ -1,18 +1,10 @@
 // ==UserScript==
 // @name        NO YELLING - instructure.com
-// @namespace   TremendousTurtle Violentmonkey Scripts
-// @description This is a userscript.
+// @namespace   https://github.com/TremendousTurtle
+// @description Sets all text on Canvas course pages to sentence case.
 // @match       *://*.instructure.com/courses/*
 // @grant       none
 // @version     process.env.VERSION
 // @author      process.env.AUTHOR
 // @require     https://cdn.jsdelivr.net/npm/js-convert-case@4.2.0/dist/js-convert-case.min.js
 // ==/UserScript==
-
-/**
- * Code here will be ignored on compilation. So it's a good place to leave messages to developers here.
- *
- * - The `@grant`s used in your source code will be added automatically by `rollup-plugin-userscript`.
- *   However you have to add explicitly those used in required resources.
- * - `process.env.VERSION` and `process.env.AUTHOR` will be loaded from `package.json`.
- */
